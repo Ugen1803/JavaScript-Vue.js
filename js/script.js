60f93ff8e76e355a3f7e389e63a0551529f76a44
Ugen <ugens1803@gmail.com>
2024-02-08 13:49:40 +0300

    hw_2_Refactoring

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Массив Товаров магазина
 const goods = [
   {
     img: '<img class="goodsImgImg" src="img/sectionProducts/shirt.png" alt="Shirt">',
@@ -24,6 +25,7 @@ const goods = [
   { price: 0 },
 ];
 
+// Объект Элемента товаров
 class GoodsItem {
   constructor({
     img = '<svg class="goodsImgImg" width="512" height="512" viewBox="0 0 512 512" style="color:currentColor" xmlns="http://www.w3.org/2000/svg" class="h-full w-full"><rect width="512" height="512" x="0" y="0" rx="30" fill="transparent" stroke="transparent" stroke-width="0" stroke-opacity="100%" paint-order="stroke"></rect><svg width="256px" height="256px" viewBox="0 0 24 24" fill="currentColor" x="128" y="128" role="img" style="display:inline-block;vertical-align:middle" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor"><g><rect width="2" height="5" x="11" y="1" fill="currentColor" opacity=".14"/><rect width="2" height="5" x="11" y="1" fill="currentColor" opacity=".29" transform="rotate(30 12 12)"/><rect width="2" height="5" x="11" y="1" fill="currentColor" opacity=".43" transform="rotate(60 12 12)"/><rect width="2" height="5" x="11" y="1" fill="currentColor" opacity=".57" transform="rotate(90 12 12)"/><rect width="2" height="5" x="11" y="1" fill="currentColor" opacity=".71" transform="rotate(120 12 12)"/><rect width="2" height="5" x="11" y="1" fill="currentColor" opacity=".86" transform="rotate(150 12 12)"/><rect width="2" height="5" x="11" y="1" fill="currentColor" transform="rotate(180 12 12)"/><animateTransform attributeName="transform" calcMode="discrete" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;30 12 12;60 12 12;90 12 12;120 12 12;150 12 12;180 12 12;210 12 12;240 12 12;270 12 12;300 12 12;330 12 12;360 12 12"/></g></g></svg></svg>',
@@ -35,6 +37,7 @@ class GoodsItem {
     this.price = price;
   }
 
+  // Метод Рендера товара
   renderItem() {
     return `
     <li class="goodsItemWrap">
@@ -61,19 +64,14 @@ class GoodsItem {
   }
 }
 
+// Объект Списка товаров
 class GoodsList {
   goods = [];
   constructor(goods) {
     this.goods = goods;
   }
 
-  calculatePrice() {
-    const calculate = this.goods.reduce((acc, { price }) => acc + price, 0);
-    document.querySelector(
-      ".calculatePrice"
-    ).innerHTML = `<h3>Общая сумма товаров на: ${calculate}$</h3>`;
-  }
-
+  // Метод Рендера списка товаров
   renderList() {
     const getGoodsList = this.goods.map((item) => {
       const goodsItem = new GoodsItem(item);
@@ -81,9 +79,24 @@ class GoodsList {
     });
     document.querySelector(".goodsList").innerHTML = getGoodsList.join("");
   }
+
+  // Метод Подсчета общей суммы:
+  calculatePrice() {
+    const calculate = this.goods.reduce((acc, { price }) => acc + price, 0);
+
+    document.querySelector(
+      ".totalPrice"
+    ).innerHTML = `<h3>Общая сумма товаров на: ${calculate}$</h3>`;
+
+    // let calculate = 0;
+    // this.goods.forEach((item) => (calculate = item.price + calculate));
+
+    // document.querySelector(
+    //   ".totalPrice"
+    // ).innerHTML = `<h3>Общая сумма товаров на: ${calculate}$</h3>`;
+  }
 }
 
 const goodsList = new GoodsList(goods);
 goodsList.renderList();
-const res = goodsList.calculatePrice();
-console.log(res);
+goodsList.calculatePrice();
